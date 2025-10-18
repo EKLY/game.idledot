304187c218cabdd872fc381c4f126bf28a98f0fa
ek <service@un-no.com>
2025-10-18 22:45:18 +0700

    add new icons and update object resize script

--- ~develop/object_resize.cmd
+++ ~develop/object_resize.cmd
@@ -1,1 +1,2 @@
-D:\project\unno.automate\.venv\Scripts\python.exe D:\project\unno.automate\idledot_object.py
+D:\project\unno.automate\.venv\Scripts\python.exe D:\project\unno.automate\idledot_building.py
+D:\project\unno.automate\.venv\Scripts\python.exe D:\project\unno.automate\idledot_icon.py
